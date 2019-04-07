918ac030010d4200125a7b4b7b009a3528e08c70
Mahdi Zareie <mahdi.elf@gmail.com>
2019-04-07 19:00:25 +0430

    add all changes

--- node-add/iptables/rm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-iptables -D INPUT  -p tcp --dport 111  -j REJECT
-iptables -D INPUT  -p udp --dport 111  -j REJECT
-iptables -D INPUT -s node-IP -p udp --dport 111  -j ACCEPT
-iptables -D INPUT -s node-IP -p tcp --dport 111  -j ACCEPT
